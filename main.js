8e797862007370d7a2046c109377d52921c00835
sanditzz <kk.pon3@gmail.com>
2023-06-16 21:41:29 +0700

    Colors for ref & hide navbar more on mobile

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ navbar = document.getElementById("navbar");
 window.addEventListener("scroll", function () {
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     if (scrollTop > lastScrollTop) {
-        navbar.style.top = "-120px";
+        navbar.style.top = "-150px";
     } else {
         navbar.style.top = "0";
     }
