edc0729ca8fc041088b6412947c3e445eabd3687
sanditzz <kk.pon3@gmail.com>
2023-06-18 15:35:33 +0700

    Carousel Dots - JavaScript

--- js/carousel.js
+++ js/carousel.js
@@ -8,7 +8,6 @@ const dots = Array.from(dotsNav.children);
 const slideWidth = slides[0].getBoundingClientRect().width;
 // console.log(slideWidth);
 
-
 // arrange the slides next to one another
 const setSlidePosition = (slide, index) => {
     slide.style.left = slideWidth * index + 'px';
@@ -22,20 +21,46 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
     targetSlide.classList.add('current-slide');
 }
 
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
 // when I click left, move slides to the left
 prevButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
     const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
 
     moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
 })
 
 // when I click right, move slides to the right
 nextButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
     const nextSlide = currentSlide.nextElementSibling;
-
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
     moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
 })
 
 // when I click the nav indicators, move to that slide
+
+dotsNav.addEventListener('click', e => {
+    // what indicator was clicked on?
+    const targetDot = e.target.closest('button');
+
+    
+    if (!targetDot) return;
+    
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+})
