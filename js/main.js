919b59c45d4c6d10d421419a87e9d1f01d5235d4
sanditzz <kk.pon3@gmail.com>
2023-06-28 21:50:11 +0700

    Lenis CDN - Smooth Scrolling

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ window.addEventListener("scroll", function () {
     lastScrollTop = scrollTop;
 })
 
+
+// Lenis Smooth Scrolling
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
