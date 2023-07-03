a59a139ef385f0ec095d19a39769417bf2bb5655
sanditzz <kk.pon3@gmail.com>
2023-07-03 21:18:44 +0700

    animate & reveal elements

--- js/reveal-element.js
+++ js/reveal-element.js
@@ -8,7 +8,7 @@ function reveal(){
 
         var windowheight = window.innerHeight;
         var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 150;
+        var revealpoint = 200;
 
         if(revealtop < windowheight - revealpoint){
             reveals[i].classList.add('active');
